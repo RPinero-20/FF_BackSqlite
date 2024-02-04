bef60781d6a808ab7d28b716cdb64c832a1971a2
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2024-02-03 21:02:30 -0400

    Add files via upload

--- build/controller/payresume.js
+++ build/controller/payresume.js
@@ -37,49 +37,11 @@ const getPayResume = (req, res) => __awaiter(void 0, void 0, void 0, function* (
     }
     try {
         if (body.orderId.length === 0) {
-            console.log("::::::::::::::: ORDER ID = 0 ::::::::::: ");
-            const productID = body.productsList.map((strID) => strID.productId);
-            const productQty = body.productsList.map((prodQty) => prodQty.quantity);
-            const productByIdPromises = productID.map((id) => __awaiter(void 0, void 0, void 0, function* () {
-                const product = yield payResume_1.cartListProducts.findByPk(id);
-                return product;
-            }));
-            const products = yield Promise.all(productByIdPromises);
-            console.log("products ::::::: ", products);
-            const productsWithQty = products.map((product, index) => {
-                const isFree = (product === null || product === void 0 ? void 0 : product.dataValues.isFree) === true;
-                const price = isFree ? 0 : parseFloat(product === null || product === void 0 ? void 0 : product.dataValues.price);
-                const priceDiscount = (0, payResumeCalc_1.calcularDescuentoProducto)(price, product === null || product === void 0 ? void 0 : product.dataValues.isOffer);
-                const productDiscount = (0, payResumeCalc_1.calcularDescuentoGlobal)(price, product === null || product === void 0 ? void 0 : product.dataValues.isOffer);
-                return Object.assign(Object.assign({ userId: parseInt(body.userId) }, product === null || product === void 0 ? void 0 : product.dataValues), { price: priceDiscount, requestedQty: productQty[index], discount: productDiscount });
-            });
-            const productsCalculated = productsWithQty.map((product) => {
-                return Object.assign(Object.assign({}, product), { price: product.price * product.requestedQty });
-            });
-            const subtotal = productsCalculated.reduce((total, product) => {
-                return total + product.price;
-            }, 0);
-            const totalDescuentoPorProducto = productsCalculated.reduce((total, product) => {
-                const descuentoPorProducto = (product === null || product === void 0 ? void 0 : product.requestedQty) * (product === null || product === void 0 ? void 0 : product.discount);
-                return total + descuentoPorProducto;
-            }, 0);
-            const ivaUsd = 16;
-            const dolarToday = 36.11;
-            const subTotalBsd = subtotal * dolarToday;
-            const iva = subTotalBsd * (ivaUsd / 100);
-            const totalBsd = subTotalBsd + iva;
-            const igtf = subtotal * (3 / 100);
-            const totalImpuesto = igtf * 0.16;
-            const totalUsd = subtotal + totalImpuesto;
-            const payResume = {
-                subtotal: parseFloat(subtotal.toFixed(2)),
-                discount: totalDescuentoPorProducto,
-                ivaUsd: 16,
-                igtf: 3,
-                totalBsd: parseFloat(totalBsd.toFixed(2)),
-                totalUsd: parseFloat(totalUsd.toFixed(2))
-            };
+            const orderRequest = body;
+            const listObjects = yield (0, payResumeCalc_1.calcProducts)(orderRequest);
+            const quoteOrder = listObjects.preOrder;
             body.userId = parseInt(body.userId);
+            const payResume = quoteOrder.payResume;
             const preOrder = Object.assign(Object.assign({}, body), { payResume: JSON.stringify({
                     payResume
                 }) });
@@ -106,9 +68,12 @@ const getPayResume = (req, res) => __awaiter(void 0, void 0, void 0, function* (
                     orderNumber: body.orderId
                 }
             });
-            console.log("::::::::::::::: EXIST ORDER::::::: ", existOrder);
             if (existOrder) {
-                res.status(403).json(Object.assign(Object.assign({}, body), { productsList: [] }));
+                const orderRequest = body;
+                const listObjects = yield (0, payResumeCalc_1.calcProducts)(orderRequest);
+                const quoteOrder = listObjects.preOrder;
+                const payResume = quoteOrder.payResume;
+                res.status(403).json(Object.assign(Object.assign({}, body), { productsList: [], payResume: payResume }));
             }
             else {
                 const newQuantity = [];
@@ -129,59 +94,11 @@ const getPayResume = (req, res) => __awaiter(void 0, void 0, void 0, function* (
                 for (const { id, unitQty } of updateStockQty) {
                     yield buyListConfirm_1.orderProductsConfirmedModel.update({ unitQty }, { where: { id } });
                 }
-                const productID = body.productsList.map((strID) => strID.productId);
-                const productQty = body.productsList.map((prodQty) => prodQty.quantity);
-                const productByIdPromises = productID.map((id) => __awaiter(void 0, void 0, void 0, function* () {
-                    const product = yield payResume_1.cartListProducts.findByPk(id);
-                    return product;
-                }));
-                const productsList = yield Promise.all(productByIdPromises);
-                const productsWithQty = productsList.map((product, index) => {
-                    const isFree = (product === null || product === void 0 ? void 0 : product.dataValues.isFree) === true;
-                    const price = isFree ? 0 : parseFloat(product === null || product === void 0 ? void 0 : product.dataValues.price);
-                    const priceDiscount = (0, payResumeCalc_1.calcularDescuentoProducto)(price, product === null || product === void 0 ? void 0 : product.dataValues.isOffer);
-                    const productDiscount = (0, payResumeCalc_1.calcularDescuentoGlobal)(price, product === null || product === void 0 ? void 0 : product.dataValues.isOffer);
-                    return Object.assign(Object.assign({ userId: parseInt(body.userId) }, product === null || product === void 0 ? void 0 : product.dataValues), { price: priceDiscount, requestedQty: productQty[index], discount: productDiscount });
-                });
-                const productsCalculated = productsWithQty.map((product) => {
-                    return Object.assign(Object.assign({}, product), { price: product.price * product.requestedQty });
-                });
-                const subtotal = productsCalculated.reduce((total, product) => {
-                    return total + product.price;
-                }, 0);
-                const totalDescuentoPorProducto = productsCalculated.reduce((total, product) => {
-                    const descuentoPorProducto = (product === null || product === void 0 ? void 0 : product.requestedQty) * (product === null || product === void 0 ? void 0 : product.discount);
-                    return total + descuentoPorProducto;
-                }, 0);
-                const ivaUsd = 16;
-                const dolarToday = 36.11;
-                const subTotalBsd = subtotal * dolarToday;
-                const iva = subTotalBsd * (ivaUsd / 100);
-                const totalBsd = subTotalBsd + iva;
-                const igtf = subtotal * (3 / 100);
-                const totalImpuesto = igtf * 0.16;
-                const totalUsd = subtotal + totalImpuesto;
-                const payResume = {
-                    subtotal: parseFloat(subtotal.toFixed(2)),
-                    discount: totalDescuentoPorProducto,
-                    ivaUsd: 16,
-                    igtf: 3,
-                    totalBsd: parseFloat(totalBsd.toFixed(2)),
-                    totalUsd: parseFloat(totalUsd.toFixed(2))
-                };
-                for (let key in payResume) {
-                    if (payResume[key] === null) {
-                        payResume[key] = 0;
-                    }
-                    ;
-                    for (let key in payResume) {
-                        if (isNaN(payResume[key])) {
-                            payResume[key] = 0;
-                        }
-                    }
-                    ;
-                }
-                ;
+                const orderRequest = body;
+                const listObjects = yield (0, payResumeCalc_1.calcProducts)(orderRequest);
+                const quoteOrder = listObjects.preOrder;
+                body.userId = parseInt(body.userId);
+                const payResume = quoteOrder.payResume;
                 const orderData = {
                     userId: body.userId,
                     productsList: JSON.stringify(body.productsList),
@@ -217,59 +134,12 @@ const getPayResume = (req, res) => __awaiter(void 0, void 0, void 0, function* (
             }
         }
         else {
-            const productID = body.productsList.map((strID) => strID.productId);
-            const productQty = body.productsList.map((prodQty) => prodQty.quantity);
-            const productByIdPromises = productID.map((id) => __awaiter(void 0, void 0, void 0, function* () {
-                const product = yield payResume_1.cartListProducts.findByPk(id);
-                return product;
-            }));
-            const products = yield Promise.all(productByIdPromises);
-            const productsWithQty = products.map((product, index) => {
-                const isFree = (product === null || product === void 0 ? void 0 : product.dataValues.isFree) === true;
-                const price = isFree ? 0 : parseFloat(product === null || product === void 0 ? void 0 : product.dataValues.price);
-                const priceDiscount = (0, payResumeCalc_1.calcularDescuentoProducto)(price, product === null || product === void 0 ? void 0 : product.dataValues.isOffer);
-                const productDiscount = (0, payResumeCalc_1.calcularDescuentoGlobal)(price, product === null || product === void 0 ? void 0 : product.dataValues.isOffer);
-                return Object.assign(Object.assign({ userId: parseInt(body.userId) }, product === null || product === void 0 ? void 0 : product.dataValues), { price: priceDiscount, requestedQty: productQty[index], discount: productDiscount });
-            });
-            const productsCalculated = productsWithQty.map((product) => {
-                return Object.assign(Object.assign({}, product), { price: product.price * product.requestedQty });
-            });
-            const subtotal = productsCalculated.reduce((total, product) => {
-                return total + product.price;
-            }, 0);
-            const totalDescuentoPorProducto = productsCalculated.reduce((total, product) => {
-                const descuentoPorProducto = (product === null || product === void 0 ? void 0 : product.requestedQty) * (product === null || product === void 0 ? void 0 : product.discount);
-                return total + descuentoPorProducto;
-            }, 0);
-            const ivaUsd = 16;
-            const dolarToday = 36.11;
-            const subTotalBsd = subtotal * dolarToday;
-            const iva = subTotalBsd * (ivaUsd / 100);
-            const totalBsd = subTotalBsd + iva;
-            const igtf = subtotal * (3 / 100);
-            const totalImpuesto = igtf * 0.16;
-            const totalUsd = subtotal + totalImpuesto;
-            const payResume = {
-                subtotal: parseFloat(subtotal.toFixed(2)),
-                discount: totalDescuentoPorProducto,
-                ivaUsd: 16,
-                igtf: 3,
-                totalBsd: parseFloat(totalBsd.toFixed(2)),
-                totalUsd: parseFloat(totalUsd.toFixed(2))
-            };
-            for (let key in payResume) {
-                if (payResume[key] === null) {
-                    payResume[key] = 0;
-                }
-                ;
-                for (let key in payResume) {
-                    if (isNaN(payResume[key])) {
-                        payResume[key] = 0;
-                    }
-                }
-                ;
-            }
-            ;
+            const orderRequest = body;
+            const listObjects = yield (0, payResumeCalc_1.calcProducts)(orderRequest);
+            const quoteOrder = listObjects.preOrder;
+            body.userId = parseInt(body.userId);
+            const payResume = quoteOrder.payResume;
+            const productsCalculated = listObjects.productsCalculated;
             const existProduct = (0, payResumeCalc_1.checkStock)(productsCalculated);
             if (existProduct.length === 0) {
                 const existProduct = body;
