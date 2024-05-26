f6d5c6e96f3ed747787beaf3a71845ce9744bcf5
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2024-05-25 22:06:58 -0400

    Add files via upload

--- build/services/storage_c.js
+++ build/services/storage_c.js
@@ -22,52 +22,13 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.savePictureToPath = void 0;
 const express_1 = require("express");
 const multer_1 = __importStar(require("multer"));
 const path_1 = require("path");
 const fs_1 = require("fs");
-function savePictureToPath(data, file) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const { code } = data;
-        console.log("savePictureToPath: code && file ", code, file);
-        const saveImage = multer_1.default.diskStorage({
-            destination: (0, path_1.join)(__dirname, '../public/assets/images/productsThumbnails/test/'),
-            filename: (req, file, cb) => {
-                cb(null, code + '-' + file.originalname.toLocaleLowerCase);
-            }
-        });
-        const fileFilter = (req, file, cb) => {
-            const filetypes = /jpeg|jpg|png|webp/;
-            const mimetype = filetypes.test(file.mimetype);
-            const extname = filetypes.test((0, path_1.extname)(file.originalname).toLowerCase());
-            if (mimetype && extname) {
-                cb(null, true);
-            }
-            else {
-                cb(new Error('El archivo debe ser una imagen.'));
-            }
-        };
-        const fileUpload = (0, multer_1.default)({
-            storage: saveImage,
-            fileFilter: fileFilter
-        }).single('picture');
-        return fileUpload;
-    });
-}
-exports.savePictureToPath = savePictureToPath;
 const saveImage = multer_1.default.diskStorage({
-    destination: (0, path_1.join)(__dirname, '../public/assets/images/productsThumbnails/'),
+    destination: 'https://raw.githubusercontent.com/RPinero-20/FF_BackNode/main/public/assets/images/productsThumbnails/',
     filename: (_req, file, cb) => {
         cb(null, file.originalname);
     }
