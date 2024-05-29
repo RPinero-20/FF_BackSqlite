9e582da4b29250f2d37fe4331ac66f78b2ffa8c7
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2024-05-28 23:40:56 -0400

    Add files via upload

--- build/models/admin.js
+++ build/models/admin.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminClients = exports.adminUsers = exports.adminProducts = exports.adminDepartmentsModel = exports.adminJobsModel = exports.adminSalesUnits = exports.adminCategory = exports.adminSections = exports.sectionsHome = void 0;
+exports.adminClients = exports.adminUsers = exports.adminProducts = exports.adminOrdersModel = exports.adminOrderStatusModel = exports.adminDepartmentsModel = exports.adminJobsModel = exports.adminStatusModel = exports.adminSalesUnits = exports.adminCategory = exports.adminSections = exports.sectionsHome = void 0;
 const sequelize_typescript_1 = require("sequelize-typescript");
 const connect_1 = __importDefault(require("../db/connect"));
 exports.sectionsHome = connect_1.default.define('ff_sections', {
@@ -55,6 +55,17 @@ exports.adminSalesUnits = connect_1.default.define('ff_sales_units', {
 }, {
     timestamps: false
 });
+exports.adminStatusModel = connect_1.default.define('ff_statuses', {
+    id: {
+        primaryKey: true,
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    name: {
+        type: sequelize_typescript_1.DataType.STRING
+    }
+}, {
+    timestamps: false
+});
 exports.adminJobsModel = connect_1.default.define('ff_jobs', {
     id: {
         primaryKey: true,
@@ -86,6 +97,79 @@ exports.adminDepartmentsModel = connect_1.default.define('ff_departments', {
     description: {
         type: sequelize_typescript_1.DataType.STRING
     },
+}, {
+    timestamps: false
+});
+exports.adminOrderStatusModel = connect_1.default.define('ff_order_statuses', {
+    id: {
+        primaryKey: true,
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    name: {
+        type: sequelize_typescript_1.DataType.STRING
+    },
+    code: {
+        type: sequelize_typescript_1.DataType.STRING
+    },
+}, {
+    timestamps: false
+});
+exports.adminOrdersModel = connect_1.default.define('ff_order_details', {
+    id: {
+        primaryKey: true,
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    userId: {
+        type: sequelize_typescript_1.DataType.STRING
+    },
+    productsList: {
+        type: sequelize_typescript_1.DataType.STRING
+    },
+    orderId: {
+        type: sequelize_typescript_1.DataType.STRING
+    },
+    isOrderConfirmed: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    payNumber: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    isOrderPaid: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    bank: {
+        type: sequelize_typescript_1.DataType.STRING
+    },
+    shippingAddress: {
+        type: sequelize_typescript_1.DataType.STRING
+    },
+    depositorName: {
+        type: sequelize_typescript_1.DataType.STRING
+    },
+    depositorPhone: {
+        type: sequelize_typescript_1.DataType.STRING
+    },
+    currency: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    subtotal: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    discount: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    ivaUsd: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    igtf: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    totalBsd: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    },
+    totalUsd: {
+        type: sequelize_typescript_1.DataType.NUMBER
+    }
 }, {
     timestamps: false
 });
