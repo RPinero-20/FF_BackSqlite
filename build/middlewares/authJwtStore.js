22c07afaa33980e2d02fa92ea5752a4840cf1822
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2024-08-15 15:50:17 -0400

    Add files via upload

--- build/middlewares/authJwtStore.js
+++ build/middlewares/authJwtStore.js
@@ -20,28 +20,43 @@ const role_1 = __importDefault(require("../models/role"));
 const uuid_1 = require("uuid");
 const clientSession_1 = require("../models/clientSession");
 const verifyToken = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    let clientUid = (0, uuid_1.v4)();
     try {
         const token = req.headers["x-access-token"];
         if (!token) {
             const payload = { id: 3, };
-            let clientUid = (0, uuid_1.v4)();
             const token = jsonwebtoken_1.default.sign(payload, config_1.default.SECRET, { expiresIn: "3h" });
             console.log("L17 New TOKEN:::: ", token);
             yield clientSession_1.guestSession.create({ uuid: clientUid, validToken: token, isLogged: false });
             res.status(200).json({ "token": token, "isLogged": false });
         }
         else {
-            jsonwebtoken_1.default.verify(token, config_1.default.SECRET);
-            next();
+            const foundToken = yield clientSession_1.guestSession.findOne({
+                where: {
+                    validToken: token
+                }
+            });
+            if (!foundToken) {
+                res.json({ Message: "Not a valid token found." });
+            }
+            console.log("  FOUND TOKEN ", foundToken);
+            const decoded = jsonwebtoken_1.default.verify(foundToken, config_1.default.SECRET);
+            req.id = decoded.id;
+            const userClient = yield admin_1.adminClients.findOne({
+                where: {
+                    uuid: decoded.id
+                }
+            });
+            if (userClient) {
+                res.status(201).json({ "userName": userClient.dataValues.name, "token": token, "isLogged": true });
+            }
+            else {
+                res.status(201).json({ "token": token, "isLogged": false });
+            }
         }
     }
     catch (error) {
-        const payload = { id: 3, };
-        let clientUid = (0, uuid_1.v4)();
-        const token = jsonwebtoken_1.default.sign(payload, config_1.default.SECRET, { expiresIn: "3h" });
-        console.log("L17 New TOKEN:::: ", token);
-        yield clientSession_1.guestSession.create({ uuid: clientUid, validToken: token, isLogged: false });
-        res.status(200).json({ "token": token, "isLogged": false });
+        return res.status(401).json({ Message: 'Not a valid token' });
     }
 });
 exports.verifyToken = verifyToken;
@@ -55,6 +70,7 @@ const verifyTokenClient = (req, res, next) => __awaiter(void 0, void 0, void 0, 
                 uuid: decoded.id
             }
         });
+        res.status(201).json({ "userName": userClient.dataValues.name, "token": token, "isLogged": true });
         next();
     }
     catch (error) {
