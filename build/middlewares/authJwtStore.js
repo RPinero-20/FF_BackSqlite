5136bdc3937fea4b86f231e9895402fad25fc90f
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2024-08-30 22:17:51 -0400

    Add files via upload

--- build/middlewares/authJwtStore.js
+++ build/middlewares/authJwtStore.js
@@ -12,13 +12,62 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.IsClient = exports.verifyTokenClient = exports.verifyToken = void 0;
+exports.IsClient = exports.verifyTokenClient = exports.verifyToken = exports.validClient = exports.validToken = exports.userInfo = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const config_1 = __importDefault(require("../config"));
 const admin_1 = require("../models/admin");
 const role_1 = __importDefault(require("../models/role"));
 const uuid_1 = require("uuid");
 const clientSession_1 = require("../models/clientSession");
+function userInfo(userData) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const decoded = jsonwebtoken_1.default.verify(userData, config_1.default.SECRET);
+        return decoded.id;
+    });
+}
+exports.userInfo = userInfo;
+function validToken(receivedToken) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("receivedToken ::: ", receivedToken);
+        try {
+            const decoded = jsonwebtoken_1.default.verify(receivedToken, config_1.default.SECRET);
+            return { "verified": true, "uuid": decoded.id };
+        }
+        catch (error) {
+            res.status(401).json({ Message: "Su sesión ha vencido, haga login de nuevo." });
+        }
+    });
+}
+exports.validToken = validToken;
+function validClient(clientId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("client ID:   ", clientId);
+        try {
+            const user = yield admin_1.adminClients.findOne({
+                where: {
+                    uuid: clientId
+                },
+                attributes: ['email', 'roles']
+            });
+            const roles = yield role_1.default.findOne({
+                where: {
+                    value: user.dataValues.roles
+                },
+                attributes: ['name', 'value']
+            });
+            if (roles.dataValues.value === 2 && user.dataValues.email) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        catch (error) {
+            res.status(500).json({ Message: "Not a valid Client." });
+        }
+    });
+}
+exports.validClient = validClient;
 const verifyToken = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const token = req.headers["x-access-token"];
@@ -70,6 +119,7 @@ const verifyTokenClient = (req, res, next) => __awaiter(void 0, void 0, void 0, 
 });
 exports.verifyTokenClient = verifyTokenClient;
 const IsClient = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("CLIENT! p0woq'0pew3", req);
     try {
         const user = yield admin_1.adminClients.findOne({
             where: {
@@ -85,10 +135,10 @@ const IsClient = (req, res, next) => __awaiter(void 0, void 0, void 0, function*
         });
         console.log("isClient ROLES::: ", roles);
         if (roles.dataValues.value === 2) {
-            next();
+            return true;
         }
         else {
-            res.status(403).json({ Message: "Moderator attributes required." });
+            return false;
         }
     }
     catch (error) {
